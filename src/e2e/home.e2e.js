600b9a3e524487cf7a322ff89cca9c77314fcbdf
陈帅 <qixian.cs@outlook.com>
2018-11-12 15:11:51 +0800

    test ci

--- src/e2e/home.e2e.js
+++ src/e2e/home.e2e.js
@@ -9,7 +9,10 @@ describe('Homepage', () => {
   it('it should have logo text', async () => {
     const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
     const page = await browser.newPage();
-    await page.goto(BASE_URL, { waitUntil: 'networkidle2' });
+    await page.goto(BASE_URL);
+    await page.waitForSelector('h1', {
+      timeout: 2000,
+    });
     const text = await page.evaluate(() => document.getElementsByTagName('h1')[0].innerText);
     expect(text).toContain('Ant Design Pro');
 
